9f4ee182daf38ffd506f9de1f26fc58e8d7d9854
jhaddad1 <71953880+jhaddad1@users.noreply.github.com>
2020-10-25 10:41:23 -0500

    Add files via upload

---
+++ quotes.py
@@ -0,0 +1,10 @@
+# Joram Nicholas William Haddad
+# Intro to Computer Science
+
+quotes = {
+    'Moe': 'A wise guy, huh?',
+    'Larry': 'Ow!',
+    'Curly': 'Nyuk nyuk!',
+    }
+stooge = 'Moe'
+print(stooge, 'says:', quotes[stooge])
